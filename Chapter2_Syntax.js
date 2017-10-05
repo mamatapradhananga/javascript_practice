44fc33fdc09e55841be82b6cc4a76df24aff0d9a
mamata pradhananga <mamata@cloudfactory.com>
2017-10-05 23:51:25 +0545

    Add chapters

---
+++ Chapter2_Syntax.js
@@ -0,0 +1,17 @@
+//JavaScript is case sensitive
+
+alert("Hello, World!!"); //correct
+Alert("Hello, World!!"); //incorrect
+
+/*
+Most things you do in JavaScript is a statement. 
+Each statement ends with semicolon
+*/
+
+alert("Hello, World!!");
+var myName = "Mamata";
+
+//JavaScript runs from top to bottom
+//Js is not sencitive to whitespace or line break
+// Write one-line comments using //
+// Write multiple line comments using /* */
---
+++ Chapter3_Variables.js
@@ -0,0 +1,48 @@
+/* 
+- Variables are heart of every programming language
+- They are used to store information
+*/
+
+// In Js Define variable with keyword var
+
+var
+
+/*
+- Then variable name
+- variable name can contain letters,numbers,underscore but cannot start with number
+- good practice to write variable name in camel case
+*/
+
+var myName2 //correct variable name
+var 2myName // incorrect variable name
+
+// Variable value: 2 ways to assign value to a variable
+
+var myVariable;
+myVariable = 12;
+
+// OR
+
+var myVariable = 12;
+
+// 
+
+
+/*
+- Just like a language has nouns, verbs, and prepositions, JavaScript has its own building blocks. 
+- Instead of calling them building blocks, however, developers call them data types.
+- There are three essential data types to know for now
+	String: Any grouping of words or numbers surrounded by single quotes: ' ... ' or double quotes " ... ".
+	Number: Any number, including numbers with decimals, without quotes: 4, 8, 1516, 23.42.
+	Boolean: This is always one of two words. Either true or false, with no quotations.
+*/
+
+int myNumber = 40; // in other language, you need to define the data type of variable
+
+//But in Js has dynamic types. A variable values can change to string,number,boolean
+// there is no need to specify the data type of variable like in other programming language
+
+var myString = "Mamata";
+var myNumber = 27;
+var myBoolean = true;
+
